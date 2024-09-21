c9e4200f255f1412ecd1a7af4a4abbe970651857
jmpm1207 <164694319+jmpm1207@users.noreply.github.com>
2024-09-21 14:26:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("buttonMessage").addEventListener("click", function() {
+    const message = document.getElementById("specialMessage");
+    message.classList.toggle("hidden");
+});
